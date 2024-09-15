702bc53afbf77153690f3020b28f53c87c023072
Uzoma Uwazurike <udu@duke.edu>
2024-09-15 16:47:12 -0400

    removed ydataprifle was too complicated

--- main2.py
+++ main2.py
@@ -1,55 +1,49 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-from ydata_profiling import ProfileReport
+
+csv_file = "Top_1000_wealthiest_people.csv"
 
 
 # Function to load data from a CSV file
 def load_data(csv_file):
     """This function loads data from a CSV file."""
-    return pd.read_csv(csv_file)
+    df = pd.read_csv(csv_file)
+    return df
 
 
 # Function to plot a pie chart of net worth distribution by industry
-def plot_pie_chart(csv_file):
+def plot_pie_chart(df):
     """This function plots the pie chart."""
-    twp = load_data(csv_file)
-
     # Group by Industry and sum Net Worth
-    industry_net_worth = twp.groupby("Industry")["Net Worth (in billions)"].sum()
+    industry_net_worth = df.groupby("Industry")["Net Worth (in billions)"].sum()
 
     # Plot the pie chart
-    industry_net_worth.plot(kind="pie", autopct="%1.3f%%")
+    industry_net_worth.plot(kind="pie", autopct="%1.1f%%")
     plt.title("Net Worth Distribution by Industry")
     plt.ylabel("")
     plt.show()
 
 
 # Function to print summary statistics
-def summary_stats(csv_file):
-    """This function loads data from a CSV and prints summary statistics."""
-    data = load_data(csv_file)
-    sum_stats = data.describe()
+def summary_stats(df):
+    """This function prints summary statistics."""
+    sum_stats = df.describe()
     print(sum_stats)
 
 
-# Function to generate a profiling report and save it as an HTML file
-def generate_summary(csv_file):
-    """
-    Generates a profiling report of any dataset and saves it as an HTML file.
-    """
-    # Load the data
-    data = load_data(csv_file)
+def grab_mean(df, col):
+    return df[col].mean()
 
-    # Generate the profiling report
-    profile = ProfileReport(data, title="Profiling Report")
 
-    # Save the report as an HTML file
-    profile.to_file("profile.html")
-    print("Profile report saved as profile.html")
+def grab_min(df, col):
+    return df[col].min()
+
+
+def grab_std(df, col):
+    return df[col].std()
 
 
 # Example usage
-csv_file = "Top_1000_wealthiest_people.csv"
-plot_pie_chart(csv_file)
-summary_stats(csv_file)
-generate_summary(csv_file)
+df = load_data(csv_file)
+plot_pie_chart(df)
+summary_stats(df)
