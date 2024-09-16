f9ff9432a7a562bbc9fb02dc0c97c8d24cd9ebad
Uzoma Uwazurike <udu@duke.edu>
2024-09-16 14:53:14 -0400

    changed df to data_frame for snake case

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ import matplotlib.pyplot as plt
 # Function to load data from a CSV file
 def load_data(csv_file):
     """This function loads data from a CSV file."""
-    df = pd.read_csv(csv_file)
-    return df
+    data_frame = pd.read_csv(csv_file)
+    return data_frame
 
 
 # Function to plot a pie chart of net worth distribution by industry
-def plot_pie_chart(df):
+def plot_pie_chart(data_frame):
     """This function plots the pie chart."""
     # Group by Industry and sum Net Worth
-    industry_net_worth = df.groupby("Industry")["Net Worth (in billions)"].sum()
+    industry_net_worth = data_frame.groupby("Industry")["Net Worth (in billions)"].sum()
 
     # Plot the pie chart
     industry_net_worth.plot(kind="pie", autopct="%1.1f%%")
@@ -25,27 +25,27 @@ def plot_pie_chart(df):
 
 
 # Function to print summary statistics
-def summary_stats(df):
+def summary_stats(data_frame):
     """This function prints summary statistics."""
-    sum_stats = df.describe()
+    sum_stats = data_frame.describe()
     print(sum_stats)
 
 
-def grab_mean(df, col):
-    return df[col].mean()
+def grab_mean(data_frame, col):
+    return data_frame[col].mean()
 
 
-def grab_min(df, col):
-    return df[col].min()
+def grab_min(data_frame, col):
+    return data_frame[col].min()
 
 
-def grab_std(df, col):
-    return df[col].std()
+def grab_std(data_frame, col):
+    return data_frame[col].std()
 
 
 # Example usage
 if __name__ == "__main__":
     csv_file = "Top_1000_wealthiest_people.csv"
-    df = load_data(csv_file)
-    plot_pie_chart(df)
-    summary_stats(df)
+    data_frame = load_data(csv_file)
+    plot_pie_chart(data_frame)
+    summary_stats(data_frame)
